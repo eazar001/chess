7574aa5eddd7c869a4d602be1024fdc71f9dc94a
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2016-09-25 13:31:39 -0700

    Implementing pawn moves.

--- Assets/Scripts/Pieces/Pawn.cs
+++ Assets/Scripts/Pieces/Pawn.cs
@@ -9,9 +9,45 @@ using System.Collections;
 /// </summary>
 public class Pawn: Piece {
 
+    bool firstMove = true;
+
     public override bool ValidMove(Vector2 pos) {
-        if(pos == (Vector2)transform.position + y1) {
-            return true;
+        Vector2 d1 = new Vector2(xMove, yMove);
+        Vector2 d2 = new Vector2(-xMove, yMove);
+        Vector2 myPos = transform.position;
+
+        if(firstMove && pos == myPos + 2*y1) {
+            RaycastHit2D up = Physics2D.Raycast(myPos, Vector2.up, 2*yMove, 1);
+
+            if(up.collider == null) {
+                firstMove = false;
+                return true;
+            }
+        }
+
+        if((pos == myPos + y1)) {
+            RaycastHit2D up = Physics2D.Raycast(myPos, Vector2.up, yMove, 1);
+
+            if(up.collider == null) {
+                firstMove = false;
+                return true;
+            }
+        } else if((pos == myPos + d1)) {
+            float distance = Mathf.Sqrt(Mathf.Pow(xMove, 2) + Mathf.Pow(yMove, 2));
+            RaycastHit2D diagRight = Physics2D.Raycast(myPos, d1, distance, 1);
+
+            if(diagRight.collider != null) {
+                firstMove = false;
+                return true;
+            }
+        } else if((pos == myPos + d2)) {
+            float distance = Mathf.Sqrt(Mathf.Pow(xMove, 2) + Mathf.Pow(yMove, 2));
+            RaycastHit2D diagLeft = Physics2D.Raycast(myPos, d2, distance, 1);
+
+            if(diagLeft.collider != null) {
+                firstMove = false;
+                return true;
+            }
         }
 
         return false;
