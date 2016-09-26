2886125a21781712ac979ea8ab9e58b9d37a6390
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2016-09-26 16:30:41 -0700

    Implementing bishop moves.

--- Assets/Scripts/Pieces/Bishop.cs
+++ Assets/Scripts/Pieces/Bishop.cs
@@ -8,7 +8,71 @@ using System;
 public class Bishop: Piece {
 
     public override bool ValidMove(Vector2 pos) {
-        return true;
+        Vector2 upDir = Vector2.up;
+        Vector2 downDir = Vector2.down;
+        Vector2 leftDir = Vector2.left;
+        Vector2 rightDir = Vector2.right;
 
+        Vector2 d1 = new Vector2(xMove, yMove);
+        Vector2 d2 = new Vector2(-xMove, yMove);
+        Vector2 myPos = transform.position;
+
+        float myX = myPos.x;
+        float myY = myPos.y;
+        float otherX = pos.x;
+        float otherY = pos.y;
+
+        float dist = Vector2.Distance(myPos, pos);
+        float xDist = myX - otherX;
+        float yDist = myY - otherY;
+
+
+        if(yDist < 0.0f) {
+            if(xDist < 0.0f) {
+                RaycastHit2D ray = Physics2D.Raycast(myPos, d1, dist, 1);
+
+                if(ray.collider == null) {
+                    return true;
+                } else {
+                    if((Vector2)ray.collider.transform.position == pos) {
+                        return true;
+                    }
+                }
+            } else if(xDist > 0.0f) {
+                RaycastHit2D ray = Physics2D.Raycast(myPos, d2, dist, 1);
+
+                if(ray.collider == null) {
+                    return true;
+                } else {
+                    if((Vector2)ray.collider.transform.position == pos) {
+                        return true;
+                    }
+                }
+            }
+        } else if(yDist > 0.0f) {
+            if(-xDist < 0.0f) {
+                RaycastHit2D ray = Physics2D.Raycast(myPos, -d1, dist, 1);
+
+                if(ray.collider == null) {
+                    return true;
+                } else {
+                    if((Vector2)ray.collider.transform.position == pos) {
+                        return true;
+                    }
+                }
+            } else if(-xDist > 0.0f) {
+                RaycastHit2D ray = Physics2D.Raycast(myPos, -d2, dist, 1);
+
+                if(ray.collider == null) {
+                    return true;
+                } else {
+                    if((Vector2)ray.collider.transform.position == pos) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
